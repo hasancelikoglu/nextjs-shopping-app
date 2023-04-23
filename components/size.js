1e507bce4df25a498f7267f23c289cf2181ed535
hasancelikoglu <hasancelikoglu92@gmail.com>
2023-04-24 00:09:02 +0300

    feat: sidebar, size div

---
+++ components/size.js
@@ -0,0 +1,27 @@
+export default function Size() {
+	return (
+		<div>
+			<h3 className="mb-3">Beden</h3>
+			<div className="grid grid-cols-3 gap-x-3 gap-y-3">
+				<div className="text-sm font-medium h-10 flex items-center justify-center border-[0.5px] rounded bg-white border-black hover:bg-black hover:text-white transition-all cursor-pointer">
+					XS
+				</div>
+				<div className="text-sm font-medium h-10 flex items-center justify-center border-[0.5px] rounded bg-white border-black hover:bg-black hover:text-white transition-all cursor-pointer">
+					S
+				</div>
+				<div className="text-sm font-medium h-10 flex items-center justify-center border-[0.5px] rounded bg-white border-black hover:bg-black hover:text-white transition-all cursor-pointer">
+					M
+				</div>
+				<div className="text-sm font-medium h-10 flex items-center justify-center border-[0.5px] rounded bg-white border-black hover:bg-black hover:text-white transition-all cursor-pointer">
+					L
+				</div>
+				<div className="text-sm font-medium h-10 flex items-center justify-center border-[0.5px] rounded bg-white border-black hover:bg-black hover:text-white transition-all cursor-pointer">
+					XL
+				</div>
+				<div className="text-sm font-medium h-10 flex items-center justify-center border-[0.5px] rounded bg-white border-black hover:bg-black hover:text-white transition-all cursor-pointer">
+					XXL
+				</div>
+			</div>
+		</div>
+	);
+}
